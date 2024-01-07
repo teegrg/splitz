1b84d8f44759cb654910f0fb79e4426968f52beb
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-01-07 13:48:35 -0500

    took out counetr

--- src/components/Visitor.jsx
+++ src/components/Visitor.jsx
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-const VisitorCounter = () => {
-  const [totalVisits, setTotalVisits] = useState(0);
+// const VisitorCounter = () => {
+//   const [totalVisits, setTotalVisits] = useState(0);
 
-  useEffect(() => {
-    // Check if the visitor has already visited
-    const hasVisited = localStorage.getItem("hasVisited");
+//   useEffect(() => {
+//     // Check if the visitor has already visited
+//     const hasVisited = localStorage.getItem("hasVisited");
 
-    if (!hasVisited) {
-      // If it's a new visit, increment the total visits
-      setTotalVisits((prevTotal) => prevTotal + 1);
+//     if (!hasVisited) {
+//       // If it's a new visit, increment the total visits
+//       setTotalVisits((prevTotal) => prevTotal + 1);
 
-      // Set a flag in localStorage to mark this visitor as visited
-      localStorage.setItem("hasVisited", "true");
-    }
-  }, []); // Empty dependency array to run the effect only on mount
+//       // Set a flag in localStorage to mark this visitor as visited
+//       localStorage.setItem("hasVisited", "true");
+//     }
+//   }, []); // Empty dependency array to run the effect only on mount
 
-  return (
-    <div className="counter">
-      {totalVisits}
-    </div>
-  );
-};
+//   return (
+//     <div className="counter">
+//       {totalVisits}
+//     </div>
+//   );
+// };
 
-export default VisitorCounter;
+// export default VisitorCounter;
--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,10 +1,10 @@
-import Visitor from "../components/Visitor"
+// import Visitor from "../components/Visitor"
 
 function Footer () {
     return (
         <div className="footer">
            © 2024 Tero Baajey, Inc 
-           <div className="counter"><Visitor/></div>
+           {/* <div className="counter"><Visitor/></div> */}
         </div>
     )
 }
