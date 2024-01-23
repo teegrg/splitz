a403afb08950223f1902312245f4395024e4dfb2
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-01-22 20:36:53 -0500

    added greeting is home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
-import Members from "../components/Members"
-function Home () {
-    return (
-        <div className="home">
-            <Members/>
-        </div>
-    )
+import React, { useState, useEffect } from 'react';
+import Members from "../components/Members";
+
+function Home() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % 4); // Use modulo to cycle through the words
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, [currentIndex]);
+
+  return (
+    <div className="home">
+      <Members />
+      <div className="transform-container">
+        {["Namaste", "Hello", "Ola", "こんにちは"].map((text, index) => (
+          <div key={index} className={`transform-word ${index === currentIndex ? 'visible' : 'hidden'}`}>
+            {text}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
