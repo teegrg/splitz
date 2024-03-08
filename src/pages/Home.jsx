79e94c6de9886abcbfd795f8f7a521d638d4e8a3
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-03-08 12:52:03 -0500

    fixed deploy buil issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,15 @@ import Members from "../components/Members";
 function Home() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const texts = ["नमस्ते", "Hello", "Ola", " བཀྲ་ཤིས་བདེ་ལེགས"]; // Declare texts before useEffect
+
   useEffect(() => {
     const timeout = setTimeout(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % texts.length); // Use modulo to cycle through the words
     }, 2000);
 
     return () => clearTimeout(timeout);
-  }, [currentIndex, texts.length]);
-
-  const texts = ["नमस्ते", "Hello", "Ola", " བཀྲ་ཤིས་བདེ་ལེགས"]; // Array of words
+  }, [currentIndex, texts.length]); // Update dependency array
 
   return (
     <div className="home">
