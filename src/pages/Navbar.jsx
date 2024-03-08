631162fedfc397499ebfc3b653a2b602cb41f729
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-03-07 21:31:44 -0500

    took out the logo icon

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
-import icon from "../components/Pic/My new design (2).png"
+// import icon from "../components/Pic/My new design (2).png"
 function Navbar () {
     return (
+        <>
         <div className="nav">
-            <div className="logo"><Link to='/'>Splitz</Link></div>
-            <img src={icon} alt="" />
+            <Link to='/' style={{ color: 'rgb(240, 222, 166)' }}><div className="logo">Splitz</div></Link>
+            {/* <img src={icon} alt="" /> */}
+            <div className="guide"><Link to='/guide' style={{ color: 'rgba(0, 0, 0, 0.661)' }}>How to use</Link></div>
         </div>
+        </>
     )
 }
 
